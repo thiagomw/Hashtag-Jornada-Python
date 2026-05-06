d41ea308e2c09f0eae66b470837cedb0c2fd0bae
Thiago Vinicius Ribeiro <thiagoribeiromw@gmail.com>
2026-05-05 21:43:46 -0300

    Jornada Python - Aula 01

---
+++ aula 1/app.py
@@ -0,0 +1,90 @@
+import pyautogui
+import time
+import pandas as pd
+
+# Passo a passo do seu aplicativo / lógica do aplicativo
+
+# Passo 1: Entrar no sistema
+# Abrir o navegador
+
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+login = "teste"
+senha = "teste"
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press("win")
+pyautogui.write("Firefox")
+pyautogui.press("enter")
+time.sleep(1)
+pyautogui.write(link)
+pyautogui.press("enter")
+# Fazer uma pausa maior para o site carregar, caso for preciso
+time.sleep(3)
+
+# Passo 2: Fazer login
+# Cliclar no campo de e-mail
+
+pyautogui.click(x=1720, y=401)
+pyautogui.write(login)
+pyautogui.press("tab")
+pyautogui.write(senha)
+pyautogui.press("enter")
+time.sleep(3)
+
+# Passo 3: Abrir a base de dados (importar o arquivo)
+
+tabela = pd.read_csv("./aula 1/produtos.csv")
+
+# Passo 4: Cadastrar 1 produto
+
+pyautogui.click(x=1687, y=274)
+pyautogui.write("MOLO000251")
+pyautogui.press("tab")
+pyautogui.write("Logitech")
+pyautogui.press("tab")
+pyautogui.write("Mouse")
+pyautogui.press("tab")
+pyautogui.write("1")
+pyautogui.press("tab")
+pyautogui.write("25.95")
+pyautogui.press("tab")
+pyautogui.write("6.50")
+pyautogui.press("tab")
+pyautogui.write("Observação teste")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+# Passo 5: Repetir o passo 4 até acabar a lista de produtos
+
+for linha in tabela.index:
+
+    codigo = str(tabela.loc[linha, "codigo"])
+    marca = str(tabela.loc[linha, "marca"])
+    tipo = str(tabela.loc[linha, "tipo"])
+    categoria = str(tabela.loc[linha, "categoria"])
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    custo = str(tabela.loc[linha, "custo"])
+    obs = str(tabela.loc[linha, "obs"])
+
+    pyautogui.click(x=1687, y=274)
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+    if obs != "NaN":
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+
+    pyautogui.press("enter") # Clicou no botão enviar
+    
+    # Voltar para o inicio da tela
+    pyautogui.scroll(3000) 
---
+++ aula 1/auxiliar.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+mouse_pos = pyautogui.position()
+time.sleep(5)
+print(mouse_pos)
